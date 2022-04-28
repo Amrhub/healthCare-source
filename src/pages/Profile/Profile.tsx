4ed72810495bc949bb145579dbd12e5a5b80526e
Nicolae Pop <nicupop729@gmail.com>
2022-04-28 17:09:58 -0400

    Build Profile component

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { Box } from '@mui/material';
 
-const Profile = () => {
-  return <div>Profile Under Construction</div>;
+import BioProfile from '../../components/Profile/BioProfile';
+import StoriesProfile from '../../components/Profile/StoriesProfile';
+
+interface IProps {  
+  name: string;
+  image: string;
+  description: string;
+  email: string;
+  phone: string;
+  address: string;
+  birthdate: string;
+  gender: string;
+  mainUser: boolean;
+  stories: any;
+  friend: boolean;
+  friendRequest: boolean;
+};
+
+
+const Profile = ({
+  name,
+  image,
+  description,
+  email,
+  phone,
+  address,
+  birthdate,
+  gender,
+  mainUser,
+  stories,
+  friend,
+  friendRequest,
+}: IProps) => {
+  return (
+    <Box sx={{ display: 'flex', gap: '50px' }}>
+      <BioProfile
+        name={name}
+        image={image}
+        description={description}
+        email={email}
+        phone={phone}
+        address={address}
+        birthdate={birthdate}
+        gender={gender}
+        mainUser={mainUser}
+        friend={friend}
+        friendRequest={friendRequest}
+      />  
+      <StoriesProfile mainUser={mainUser} stories={stories} />
+ 
+    </Box>
+  );
 };
 
 export default Profile;
