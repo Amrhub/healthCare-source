c2b1eddd26bc20a39c790a78185a71f722464d85
Nicolae Pop <nicupop729@gmail.com>
2022-04-28 17:14:11 -0400

    Add link to comunity user cards

--- src/pages/Community/FriendRequests.tsx
+++ src/pages/Community/FriendRequests.tsx
@@ -1,31 +1,42 @@
 import { Check } from '@mui/icons-material';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import { Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 import FriendCard from '../../components/FriendCard/FriendCard';
+import { userRoutes } from '../../Routes/Routes';
 const FriendRequests = () => {
   return (
     <Grid container columnSpacing={2} rowSpacing={3}>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
-        />
+        <Link
+          to={userRoutes.profile.community}
+          style={{ textDecoration: 'none', color: '#4264D0' }}
+        >
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
+          />
+        </Link>
       </Grid>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
-        />
+        <Link to={userRoutes.profile.community} style={{ textDecoration: 'none', color: '#4264D0' }}>
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
+          />
+        </Link>
       </Grid>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
-        />
+        <Link to={userRoutes.profile.community} style={{ textDecoration: 'none', color: '#4264D0' }}>
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<Check color="primary" />, <DeleteOutlinedIcon color="error" />]}
+          />
+        </Link>
       </Grid>
     </Grid>
   );
--- src/pages/Community/Friends.tsx
+++ src/pages/Community/Friends.tsx
@@ -1,31 +1,39 @@
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import { Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 import FriendCard from '../../components/FriendCard/FriendCard';
+import { userRoutes } from '../../Routes/Routes';
 
 const Friends = () => {
   return (
     <Grid container columnSpacing={2} rowSpacing={3}>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<PersonRemoveIcon color="error" />]}
-        />
+        <Link to={userRoutes.profile.community} style={{ textDecoration: 'none', color: '#4264D0' }}>
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<PersonRemoveIcon color="error" />]}
+          />
+        </Link>
       </Grid>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<PersonRemoveIcon color="error" />]}
-        />
+        <Link to={userRoutes.profile.community} style={{ textDecoration: 'none', color: '#4264D0' }}>
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<PersonRemoveIcon color="error" />]}
+          />
+        </Link>
       </Grid>
       <Grid item md={6}>
-        <FriendCard
-          userName="Mohamed Adel"
-          userAvatar="https://i.pravatar.cc/300?img=3"
-          icons={[<PersonRemoveIcon color="error" />]}
-        />
+        <Link to={userRoutes.profile.community} style={{ textDecoration: 'none', color: '#4264D0' }}>
+          <FriendCard
+            userName="Mohamed Adel"
+            userAvatar="https://i.pravatar.cc/300?img=3"
+            icons={[<PersonRemoveIcon color="error" />]}
+          />
+        </Link>
       </Grid>
     </Grid>
   );
