119759248d86d0648b4d5de967b19b59acba58cd
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-05-19 03:58:04 +0200

    setup: usersReducer structure

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import { userReducer } from './users/users';
+
+export const baseUrl = 'http://localhost:3000/';
+export const apiVersion = 'api/v1/';
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
   },
 });
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/redux/users/users.ts
+++ src/redux/users/users.ts
@@ -1,10 +1,88 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-export const userReducer = createSlice({
+import { apiVersion, baseUrl } from '../configureStore';
+
+export const createDoctor = createAsyncThunk(
+  'users/createDoctor',
+  async (doctorFormData: FormData) => {
+    const response = await fetch(`${baseUrl}${apiVersion}doctors`, {
+      method: 'POST',
+      body: doctorFormData,
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  },
+);
+
+interface IState {
+  id: number;
+}
+
+interface RoleDoctorInfo {
+  specialization: string;
+  experience: number;
+  salary: number;
+  certificates: string[];
+}
+
+interface RolePatientInfo {
+  weight: number;
+  height: number;
+  covid: boolean;
+  hypertension: boolean;
+  diabetes: boolean;
+  otherDiseases: string;
+}
+
+type RoleInfo = RoleDoctorInfo | RolePatientInfo;
+
+const userSlice = createSlice({
   name: 'user',
   initialState: {
-    user: {},
+    user: {
+      userInfo: {
+        id: 0,
+        profilePic: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        role: '',
+        referenceId: '',
+        bio: '',
+        phone: '',
+        address: '',
+        gender: '',
+        birthDate: '',
+        age: 0,
+        roleInfo: {} as RoleInfo,
+      },
+      loading: 'idle',
+      authorizationToken: '',
+    },
   },
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(createDoctor.fulfilled, (state, { payload }) => {
+      state.user.userInfo.referenceId = payload.id;
+      state.user.userInfo.roleInfo = {
+        specialization: payload.specialization,
+        experience: payload.years_experience,
+        salary: payload.salary,
+        certificates: payload.certificates,
+      };
+    });
+
+    builder.addCase(createDoctor.pending, (state, action) => {
+      state.user.loading = 'pending';
+    });
+
+    builder.addCase(createDoctor.rejected, (state, action) => {
+      state.user.loading = 'rejected';
+    });
+  },
 });
+
+export const userReducer = userSlice.reducer;
+// export const {} = userSlice.actions;
